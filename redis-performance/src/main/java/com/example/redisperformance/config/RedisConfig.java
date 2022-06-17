a3447c4ae98a96a71954af75599fb447c667fbf5
mskim <mskim@11h11m.com>
2022-06-17 11:35:46 +0900

    git sync

---
+++ redis-performance/src/main/java/com/example/redisperformance/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.redisperformance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        if(redisConnectionFactory instanceof LettuceConnectionFactory) {
+//            ((LettuceConnectionFactory) redisConnectionFactory).setShareNativeConnection(false);
+        }
+
+        var redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return redisTemplate;
+    }
+}
---
+++ redis-performance/src/main/java/com/example/redisperformance/lock/Nft.java
@@ -0,0 +1,30 @@
+package com.example.redisperformance.lock;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@RedisHash("nft")
+public class Nft {
+
+    @Id
+    private Long id;
+    private String name;
+    private Long price;
+    private int tokenAddress;
+    private boolean isMint;
+
+    public Nft(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.tokenAddress = this.hashCode();
+    }
+
+    public String getHashKey() {
+        return "nft:" + this.id;
+    }
+}
