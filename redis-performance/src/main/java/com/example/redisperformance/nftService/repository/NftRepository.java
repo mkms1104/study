9e08aa9789e9e73228c37c390569bb03f20036a7
mskim <mskim@11h11m.com>
2022-07-12 15:06:24 +0900

    redis 테스트 모듈 분리

--- redis-performance/src/main/java/com/example/redisperformance/nftService/repository/NftRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.redisperformance.nftService.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NftRepository extends CrudRepository<Nft, Long> {
-
-}
