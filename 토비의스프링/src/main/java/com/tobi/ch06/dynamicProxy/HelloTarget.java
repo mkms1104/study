2ca404cd8060feb3fb68cc3dcb34f6321cd4ff96
mskim <mskim@11h11m.com>
2022-07-25 00:12:45 +0900

    토비의 스프링 6장

--- 토비의스프링/src/main/java/com/tobi/ch06/dynamicProxy/HelloTarget.java
+++ 토비의스프링/src/main/java/com/tobi/ch06/dynamicProxy/HelloTarget.java
@@ -1,7 +1,12 @@
 package com.tobi.ch06.dynamicProxy;
 
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
 public class HelloTarget implements Hello {
     @Override
+    @Transactional
     public String sayHello(String name) {
         return "Hello " + name;
     }
