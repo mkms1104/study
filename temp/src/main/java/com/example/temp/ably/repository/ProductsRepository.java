038cf9e409ae8f840b021cc0c10b8adbc05ab3b5
mskim <mskim@11h11m.com>
2022-08-15 20:24:49 +0900

    모듈 간 build.gradle 파일 내 중복 코드 제거

--- temp/src/main/java/com/example/temp/ably/repository/ProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.temp.ably.repository;
-
-import com.example.temp.ably.domain.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Products, Long> {
-}
--- temp/src/main/java/com/example/temp/ably/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.temp.ably.repository;
-
-import com.example.temp.ably.domain.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
